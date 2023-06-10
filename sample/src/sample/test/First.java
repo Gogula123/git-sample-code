52df2a28f11a844d64d1958750055437fe6b0342
Gogula123 <136161045+Gogula123@users.noreply.github.com>
2023-06-10 15:29:39 +0530

    Changed from Remote
    Changed from Remote

--- sample/src/sample/test/First.java
+++ sample/src/sample/test/First.java
@@ -6,6 +6,7 @@ public class First {
 		// TODO Auto-generated method stub
 		System.out.println("*****************");
 		System.out.println("Changed user name");
+		System.out.println("Changed from remote");
 	}
 
 }
